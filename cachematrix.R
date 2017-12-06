63c9b91e2f99d10933e47f76937f419ea3b6b437
Melabady <melabady@gmail.com>
2017-12-06 13:51:57 +0200

    the solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cahing the the inverse of a matrix,
+## Rather than calculating the same inverted matrix 
+## everytime for the same object
 
-## Write a short comment describing this function
+
+## Takes a matrix and creates a list of four functions:
+## get - get the inputed matrix (Argument x).
+## set - change the value of the matrix.
+## getinverse - get the cached inverted matrix of x.
+## setinverse - cache the inverted matrix of x.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## return the cached iverted matrix if exists,
+## otherwise calculate the iverse and cashe the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+    if(!is.null(inv)) {
+            message("getting cached inversed matrix")
+            return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
